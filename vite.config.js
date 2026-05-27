d822673da970ce45db8994e838e308a616e10c9c
Joaco_Vrz <joaco0017@hotmail.com>
2026-05-27 17:00:16 -0600

    copyPublicDir añadido

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    copyPublicDir: true,
+  },
   server: {
     proxy: {
       '/api': {
